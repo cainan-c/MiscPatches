a209fc192a6af27964d913a7246a89c3e534fc82
Cainan <cainancorlett@outlook.com>
2022-04-15 21:53:12 +0100

    Add files via upload

---
+++ CustomShougou.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+
+namespace MiscPatches
+{
+    public class CustomShougou
+    {
+        [HarmonyPatch(typeof(WordDataInterface))]
+        [HarmonyPatch(MethodType.Constructor)]
+        [HarmonyPatch(new[] { typeof(string), typeof(string) })]
+        [HarmonyPostfix]
+        private static void WordDataInterface_Postfix(WordDataInterface __instance, string path, string language)
+        {   
+            var TextInfoAccessors = __instance.wordListInfoAccessers;
+
+            string shougouKey = "shougou_000";
+            string shougouText = Plugin.configCustomShougouText.Value;
+            int fontType = Plugin.configCustomShougouLang.Value;
+
+            TextInfoAccessors.Insert(0, new WordDataInterface.WordListInfoAccesser(shougouKey, shougouText, fontType));
+            TextInfoAccessors.Insert(0, new WordDataInterface.WordListInfoAccesser("customize_shougou_blank", "Custom Title: "+shougouText, fontType));
+            //TextInfoAccessors.Insert(0, new WordDataInterface.WordListInfoAccesser("customize_shougou_blank_description", "Current Custom Title: "+shougouText, fontType));
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace MiscPatches
+{
+    [BepInPlugin("com.swigz.miscpatches", "Misc Patches", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> configOfflineSaveLoad;
+        public static ConfigEntry<bool> configQuickRestart;
+        public static ConfigEntry<bool> configQuickQuitSong;
+
+        public static ConfigEntry<bool> configCustomShougou;
+        public static ConfigEntry<string> configCustomShougouText;
+        public static ConfigEntry<int> configCustomShougouLang;
+        private void Awake()
+        {
+
+        configOfflineSaveLoad = Config.Bind("General.Patches",
+                        "OfflineSaveLoad",
+                        false,
+                        "Loads local save files even when offline. Bypasses the log-in checks. Requires the FreeLocalSaves Plugin.");
+
+        configQuickRestart = Config.Bind("General.Patches",
+                        "QuickRestart",
+                        false,
+                        "Hit \"Backspace\" on your keyboard to quickly restart a song.");
+
+        configQuickQuitSong = Config.Bind("General.Patches",
+                        "QuickQuitSong",
+                        false,
+                        "Hit \"Escape\" on your keyboard to quickly quit a song and return to Song Select.");
+
+        configCustomShougou = Config.Bind("General.CustomTitle",
+                        "CustomShougou",
+                        false,
+                        "Enable the Custom Title Patch.");
+
+        configCustomShougouText = Config.Bind("General.CustomTitle",
+                        "CustomShougouText",
+                        "",
+                        "Set a Custom Title");
+
+        configCustomShougouLang = Config.Bind("General.CustomTitle",
+                        "CustomShougouLang",
+                        1,
+                        "Set the font to use.\nJp = 0\nEn,Fr,It,De,Es = 1\nTc = 2\nSc = 3\nKr = 4");
+
+
+            var instance = new Harmony("Misc Patches");
+
+            if (Plugin.configOfflineSaveLoad.Value)
+                instance.PatchAll(typeof(OfflineSaveLoad));
+
+            if (Plugin.configQuickRestart.Value)
+                instance.PatchAll(typeof(QuickRestart));
+
+            if (Plugin.configQuickQuitSong.Value)
+                instance.PatchAll(typeof(QuickQuitSong));
+
+            if (Plugin.configQuickQuitSong.Value)
+                instance.PatchAll(typeof(CustomShougou));
+
+
+
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {"com.swigz.miscpatches"} is loaded!");
+        }
+    }
+}
